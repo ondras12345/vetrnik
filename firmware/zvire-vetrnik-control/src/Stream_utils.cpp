9599f1430333f7e610b740acd6e60fdd1e576968
Ondřej Sluka <ondras12345@gmail.com>
2022-11-23 18:56:42 +0100

    Change format of conf prinout to work like export

--- firmware/zvire-vetrnik-control/src/Stream_utils.cpp
+++ firmware/zvire-vetrnik-control/src/Stream_utils.cpp
@@ -12,18 +12,19 @@ void stream_print_MAC(Stream *response, uint8_t MAC[6])
 
 void stream_print_settings(Stream *response, settings_t s)
 {
-    response->print("ETH_MAC: ");
+    response->print("conf ETH_MAC ");
     stream_print_MAC(response, s.ETH_MAC);
     response->println();
 
-    response->print("ETH_IP: ");
+    response->print("conf ETH_IP ");
     response->println(IPAddress(s.ETH_IP));
 
-    response->print("MQTTserver: ");
+    response->print("conf MQTTserver ");
     response->println(IPAddress(s.MQTTserver));
 
-    response->printf("MQTTuser: \"%s\"\r\n", s.MQTTuser);
+    response->print("conf MQTTuser ");
+    response->println(s.MQTTuser);
 
-    response->printf("MQTTpassword: \"%s\"\r\n", s.MQTTpassword);
-
+    response->print("conf MQTTpassword ");
+    response->println(s.MQTTpassword);
 }
