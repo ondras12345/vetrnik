c949f08c28eb0edd7da7206030a32c8dcf575095
Ondřej Sluka <ondras12345@gmail.com>
2023-07-21 15:05:18 +0200

    [control] [LISP] Set RPM high / low constants

--- firmware/zvire-vetrnik-control/lisp/30-control-conf.lisp
+++ firmware/zvire-vetrnik-control/lisp/30-control-conf.lisp
@@ -1,7 +1,7 @@
 ; configuration
 (= SA 15) ; step authority
-(= RL 10) ; RPM low
-(= RH 800) ; RPM high
+(= RL 80) ; RPM low
+(= RH 1000) ; RPM high, TODO
 (= T0M 85) ; max t0
 (= T0S 75) ; t0 to start after T0M
 (= PS 60) ; only start the pump if t0 is higher than this
