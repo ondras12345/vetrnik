1f9037f9fccc2eb2533731b000e244e5c0dec279
Ondřej Sluka <ondras12345@gmail.com>
2024-06-26 12:01:30 +0200

    [LISP] Check power mode in btn1_long

--- firmware/vetrnik-control/lisp/40-buttons.lisp
+++ firmware/vetrnik-control/lisp/40-buttons.lisp
@@ -6,11 +6,17 @@
 
 
 (= btn1_long (fn ()
-  (= d 0)
-  (= t0 (ds18 0))
-  (swen)
-  (ctrls "strategy" "control_lisp")
-  (pwrs "mode" pwr_start)
+  ; check power mode to prevent mode3 error on power board
+  (if
+    (is (pwrg "mode") pwr_shorted) (do
+      (= d 0)
+      (= t0 (ds18 0))
+      (swen)
+      (ctrls "strategy" "control_lisp")
+      (pwrs "mode" pwr_start)
+    )
+    ; else: do nothing
+  )
 ))
 
 
