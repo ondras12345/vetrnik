895b9c4ec47136a244d554ed7c67654fc8c4431f
Ondřej Sluka <ondras12345@gmail.com>
2025-02-13 20:43:59 +0100

    [LISP] Improve MPPT
    ... based on SILsimplified simulations.
    However, the original algorithm might have some advantages.
    It might be able to operate with a higher sampling rate.

--- firmware/vetrnik-control/lisp/30-control-conf.lisp
+++ firmware/vetrnik-control/lisp/30-control-conf.lisp
@@ -1,8 +1,10 @@
 ; configuration
-(= SA 2) ; step authority
+(= SA 8) ; step authority
 (= RLC 100) ; minimum RPM to switch contactor
-(= RL 250) ; RPM low
-(= RH 1000) ; RPM high, TODO
+(= RL 50) ; RPM low
+(= RH 2000) ; RPM high, TODO
 (= T0M 85) ; max t0
 (= T0S 75) ; t0 to start after T0M
 (= PS 60) ; only start the pump if t0 is higher than this
+(= Ts 8.0) ; MPPT sampling period in seconds
+;(= J (* 1.2 0.0109662)) ; moment of inertia, adjusted for rpm instead of rad/s
