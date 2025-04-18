69b36a7b70f11f03006a182ae18bf7f4069e2a13
Ondřej Sluka <ondras12345@gmail.com>
2025-04-18 15:42:11 +0200

    [lisp] Improve HCS / P&O controller implementation

--- firmware/vetrnik-control/lisp/30-control-conf.lisp
+++ firmware/vetrnik-control/lisp/30-control-conf.lisp
@@ -1,10 +1,10 @@
 ; configuration
-(= SA 8) ; step authority
+(= SA 2) ; step authority
 (= RLC 100) ; minimum RPM to switch contactor
-(= RL 50) ; RPM low
-(= RH 2000) ; RPM high, TODO
+(= RL 100) ; RPM low: leave the turbine unloaded until this RPM
+(= RH 2000) ; RPM high: do not let the turbine get past this
 (= T0M 85) ; max t0
-(= T0S 75) ; t0 to start after T0M
+(= T0S 75) ; t0 to restart at after shutdown due to T0M
 (= PS 60) ; only start the pump if t0 is higher than this
-(= Ts 8.0) ; MPPT sampling period in seconds
+(= Ts 7.5) ; MPPT sampling period in seconds
 ;(= J (* 1.2 0.0109662)) ; moment of inertia, adjusted for rpm instead of rad/s
